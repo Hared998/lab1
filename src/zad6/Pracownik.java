053b459ad54ae9ef3fefbff9ff1e7600ff09fb1d
Hared998 <adrianmerchel@gmail.com>
2024-04-03 20:00:41 +0200

    Update zad1,2,6

--- src/zad6/Pracownik.java
+++ src/zad6/Pracownik.java
@@ -1,9 +1,9 @@
 package zad6;
 
 public class Pracownik {
-    SpedzanieWolnegoCzasu spedzanieWolnegoCzasu;
-    Dojezdzac dojezdzac;
-    Pracowac pracowac;
+    private SpedzanieWolnegoCzasu spedzanieWolnegoCzasu;
+    private Dojezdzac dojezdzac;
+    private Pracowac pracowac;
     String zawod;
     String imie;
     public Pracownik( Pracowac pracowac, SpedzanieWolnegoCzasu spedzanieWolnegoCzasu, Dojezdzac dojezdzac, String zawod, String imie)
@@ -15,5 +15,13 @@ public class Pracownik {
         this.imie = imie;
         System.out.println("Dodano pracownika: " + imie + ", " + zawod);
     }
-
+    public void dojezdaj (){
+        dojezdzac.dojezdzaj();
+    }
+    public void pracuj (){
+        pracowac.pracuj();
+    }
+    public void spedzajWolnyCzas (){
+        spedzanieWolnegoCzasu.spedzajWolnyCzas();
+    }
 }
--- src/zad6/mainPracownik.java
+++ src/zad6/mainPracownik.java
@@ -3,9 +3,9 @@ package zad6;
 public class mainPracownik {
     public static void main(String[] args) {
         Pracownik mechanik = new Pracownik(new NaprawaSamochodow(), new Silownia(), new Samochod(), "Mechanik", "Adrian");
-        mechanik.dojezdzac.dojezdzaj();
-        mechanik.pracowac.pracuj();
-        mechanik.spedzanieWolnegoCzasu.spedzajWolnyCzas();
+        mechanik.dojezdaj();
+        mechanik.pracuj();
+        mechanik.spedzajWolnyCzas();
 
     }
 }
