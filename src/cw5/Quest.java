ae3f18fa0dc435bd936b73d805a6de2430c722bc
Hared998 <adrianmerchel@gmail.com>
2024-06-09 15:00:27 +0200

    Update Quest.java

--- src/cw5/Quest.java
+++ src/cw5/Quest.java
@@ -2,11 +2,14 @@ public class Quest implements Observer {
     private String itemName;
     private int requiredCount;
     private int currentCount;
+    private Subject subject
 
-    public Quest(String itemName, int requiredCount) {
+    public Quest(String itemName, int requiredCount, Subject subject) {
         this.itemName = itemName;
         this.requiredCount = requiredCount;
         this.currentCount = 0;
+        this.subject = subject;
+        this.subject.registerObserver(this);
     }
 
     @Override
@@ -20,6 +23,8 @@ public class Quest implements Observer {
     private void checkCompletion() {
         if (currentCount >= requiredCount) {
             System.out.println("Zadanie ukończone: Zbierz " + requiredCount + " " + itemName + "(y)");
+
+            this.subject.removeObserver(this);
             // Here you would remove this quest from the observer list in Inventory
         } else {
             System.out.println("Postęp zadania: " + currentCount + "/" + requiredCount + " " + itemName + "(y)");
